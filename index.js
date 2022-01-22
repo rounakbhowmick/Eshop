731149c2fbb735f07231b24d979ceb9c18897ec2
rounakbhowmick <rounak.bhowmick@neutrinos.co>
2022-01-22 20:09:12 +0530

    schema added

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+const userRoute=require("./routes/user")
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log("db Success")).catch((e)=>console.log(e));
+
+// app.get("/pr",()=>{
+//     console.log("ggg");
+// })
+app.use(express.json())
+app.use("/api/user",userRoute)
+app.listen(process.env.PORT ||5000,()=>{
+    console.log("Server running")
+})
